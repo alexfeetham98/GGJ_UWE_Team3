b9b8c9251b4784ae5c981a1bff18d2091da0faeb
Conrad Wilson <conrad2.wilson@live.uwe.ac.uk>
2021-01-31 11:48:36 +0000

    WIP: Adding gem switching UI

--- GGJ_UWE_Team3_UnityProject/Assets/Scripts/GemSwitcher.cs
+++ GGJ_UWE_Team3_UnityProject/Assets/Scripts/GemSwitcher.cs
@@ -2,18 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class GemSwitcher : MonoBehaviour
 {
+    [SerializeField] private RectTransform gemWheel;
+    [SerializeField] private RectTransform slot1;
+    [SerializeField] private RectTransform slot2;
+    [SerializeField] private RectTransform slot3;
+    [SerializeField] private RectTransform slot4;
+
     private Inventory inv;
     private bool canSwitch;
     [SerializeField] private int selectedGemIndex;
+    private Vector3 selectedSlotScale;
+    private Vector3 nonSelectedSlotScale;
+    private bool updatingLerpScaler;
+    private float lerpScaler;
+    [SerializeField] private float lerpScalerSpeed;
 
     private void Awake()
     {
         inv = GetComponent<Inventory>();
         canSwitch = false;
         selectedGemIndex = -1;
+        selectedSlotScale = new Vector3(0.26f, 0.26f, 0.26f);
+        nonSelectedSlotScale = new Vector3(0.21f, 0.21f, 0.21f);
+        updatingLerpScaler = false;
+        lerpScaler = 0;
     }
 
     private void Update()
@@ -23,8 +39,28 @@ public class GemSwitcher : MonoBehaviour
         CheckIfCanSwitch();
     }
 
+    private void UpdateLerpScaler()
+    {
+        if (updatingLerpScaler && lerpScaler < 1)
+        {
+            lerpScaler += 0 * lerpScalerSpeed * Time.deltaTime;
+        }
+        else if (lerpScaler >= 1)
+        {
+            lerpScaler = 1;
+            updatingLerpScaler = false;
+        }
+
+        if (!updatingLerpScaler)
+        {
+            lerpScaler = 0;
+        }
+    }
+
     private void UpdateGemVisuals()
     {
+
+
         if (inv.gemInv.Count == 0)
         {
             selectedGemIndex = -1;
@@ -40,6 +76,10 @@ public class GemSwitcher : MonoBehaviour
         if (inv.gemInv.Count == 1)
         {
             selectedGemIndex = 0;
+            slot1.localScale = Vector3.Slerp(nonSelectedSlotScale, selectedSlotScale, lerpScaler);
+            slot2.localScale = nonSelectedSlotScale;
+            slot3.localScale = nonSelectedSlotScale;
+            slot4.localScale = nonSelectedSlotScale;
         }
     }
 
@@ -68,6 +108,8 @@ public class GemSwitcher : MonoBehaviour
             {
                 selectedGemIndex = 0;
             }
+
+            updatingLerpScaler = true;
         }
         else if (value.Get<float>() < 0 && canSwitch)
         {
